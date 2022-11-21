3e526da175bd7f304d7f389aabd9f8dd4325a5a4
Tarin <tarin@Zipcoders-MacBook-Pro-5.local>
2022-11-21 01:09:29 -0500

    finished avg integer arrays and array lists

--- src/main/java/AverageIntArrays.java
+++ src/main/java/AverageIntArrays.java
@@ -3,15 +3,20 @@ public class AverageIntArrays {
 // Complete these methods
 
     public int count(int[] aa) {
-        return 0;
+        return aa.length;
     }
 
     public int sum(int[] aa) {
-        return 0;
+        int total = 0;
+        for (int i = 0; i < aa.length; i++) {
+            total += aa[i];
+        }
+        return total;
     }
 
     public int average(int[] aa) {
-        return 0;
+
+        return sum(aa)/count(aa);
     }
 
 }
--- src/main/java/AverageIntegerLists.java
+++ src/main/java/AverageIntegerLists.java
@@ -4,14 +4,18 @@ public class AverageIntegerLists {
 
 
     public int count(ArrayList<Integer> aa) {
-        return 0;
+        return aa.size();
     }
 
     public int sum(ArrayList<Integer> aa) {
+        int total = 0;
+        for (int i = 0; i < aa.size(); i++) {
+            total += aa.get(i);
+        }
         return 0;
     }
 
     public int average(ArrayList<Integer> aa) {
-        return 0;
+        return sum(aa)/count(aa);
     }
 }
